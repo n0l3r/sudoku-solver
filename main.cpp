de542f1e4c2161758f06d337aebccff195cdc081
n0l3r <naufal.120140044@student.itera.ac.id>
2022-06-06 19:01:26 +0700

    sudoku solver with backtracking algorithm

--- main.cpp
+++ main.cpp
@@ -2,6 +2,69 @@
 using namespace std;
 
 #define SIZE 9
+#define matrix vector<vector<int>>
+
+bool isSafe(matrix board, int row, int col, int val) {
+    for(int j = 0; j < board.size(); j++) {
+        if(board[row][j] == val) return false;   // cek baris
+    }
+    for(int j = 0; j < board.size(); j++) {
+        if(board[j][col] == val) return false;   // cek kolom
+    }
+
+    int square = sqrt(board.size());             // cek kotak yang sedang dikerjakan
+    row -= (row % square);
+    col -= (col % square);
+
+    for(int i = row; i < row + square; i++) {
+        for(int j = col; j < col + square; j++) {
+            if(board[i][j] == val) return false;
+        }
+    }
+
+    return true;
+}
+
+bool solver(matrix &board) {
+    int row, col;
+    bool isZero = 0;
+
+    for(int i = 0; i < board.size(); i++) {
+        for(int j = 0; j < board.size(); j++) {
+            if(board[i][j] == 0) {
+                row = i;
+                col = j;
+                isZero = true;
+                break;
+            }
+        }
+        if(isZero) break;
+    }    
+
+    if(!isZero) return true;
+
+    for(int i = 1; i <= board.size(); i++) {
+        if(isSafe(board, row, col, i)) {
+            board[row][col] = i;
+            if(solver(board) == true){
+                return true;
+            } else{
+                board[row][col] = 0;
+            }
+        }
+    }
+
+    return false;
+}
+
+void printBoard(matrix &board) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 int main() {
 
@@ -22,7 +85,7 @@ int main() {
    -------------------------
     */
 
-    vector<vector<int>> board(SIZE, vector<int>(SIZE, 0)); // board 9x9 dengan isi 0
+    matrix board(SIZE, vector<int>(SIZE, 0)); // board 9x9 dengan isi 0
     string _inFileName, _outFileName;
 
     cout << "Masukkan nama file input : ";
@@ -41,14 +104,10 @@ int main() {
         }
     }
 
-    // mencetak isi board ke file output
-    for(int i = 0; i < SIZE; i++) {
-        for(int j = 0; j < SIZE; j++) {
-            cout << board[i][j] << " ";
-        }
-        cout << endl;
-    }
+    solver(board);
 
+    // mencetak isi board ke file output
+    printBoard(board);
     fclose(stdin); // menutup file input
     fclose(stdout); // menutup file output
     return 0;
